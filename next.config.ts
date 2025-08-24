bc75b14cb0c03d0ebb1c1129c71af8e15b17a7a6
Mohamed Alkayal <mohamedalkayal@Mohameds-MacBook-Air.local>
2025-08-24 17:02:15 +0300

    disable linting rules while building the app

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['is1-ssl.mzstatic.com', 'is2-ssl.mzstatic.com', 'is3-ssl.mzstatic.com', 'is4-ssl.mzstatic.com', 'is5-ssl.mzstatic.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
